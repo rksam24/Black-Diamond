695839aaa85c5c9d7d0e2638bbe9dea3dbd2321c
rksam24 <r.ksambhariya1998@gmail.com>
2021-05-29 19:12:35 +0530

    bike photo

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,7 +34,6 @@ if(mybutton) {
 let header = $(`
 <nav class="fixed-top black">
 <div class="nav-wrapper">
-    <a href="#!" class="brand-logo"><img src="assets/images/YM.svg"></a>
     <a href="#" data-target="mobile-demo" class="white-text sidenav-trigger"><i
             class="material-icons">menu</i></a>
     <ul class="right hide-on-med-and-down">
